6215df02a61971adaccfe2f0d9965f58ff8104eb
Serhii-Buha <serhii.buga@gmail.com>
2023-06-10 11:20:41 +0300

    refactoring component Filter, add useDispatch, getVisibleContact

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,21 +1,18 @@
-import PropTypes from 'prop-types';
-import { Form } from 'react-bootstrap';
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import { useDispatch } from 'react-redux';
+import { getVisibleContact } from 'redux/filter/filterSlice';
 
-export const Filter = ({ value, onChange }) => {
+export const Filter = () => {
+  const dispatch = useDispatch();
+
   return (
     <Form.Group controlId="formFilter">
       <Form.Label className="mb-2 fs-5">Find contacts by name</Form.Label>
       <Form.Control
         type="search"
-        name="filter"
-        value={value}
-        onChange={onChange}
+        onChange={e => dispatch(getVisibleContact(e.target.value))}
       ></Form.Control>
     </Form.Group>
   );
-};
-
-Filter.propTypes = {
-  value: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
 };
