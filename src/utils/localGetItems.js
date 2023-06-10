f6c9afe7fb3fc29f609e61cea06e26797096d31d
Serhii-Buha <serhii.buga@gmail.com>
2023-06-10 11:29:22 +0300

    delete file localGetItems

--- src/utils/localGetItems.js
+++
@@ -1,9 +0,0 @@
-export const localGetItems = () => {
-  const storedContacts = localStorage.getItem('contacts');
-
-  if (storedContacts !== null) {
-    const parsedContacts = JSON.parse(storedContacts);
-    return parsedContacts;
-  }
-  return [];
-};
