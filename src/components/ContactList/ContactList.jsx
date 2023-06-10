d80d13ed2a74df846a2c1369ad32b2ebfafec356
Serhii-Buha <serhii.buga@gmail.com>
2023-06-10 11:19:19 +0300

    refactoring component ContactList, add useDispatch, useSelector

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,30 +1,30 @@
-import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
 import { List, Item, Button } from 'components/ContactList/ContactList.styled';
 import { ContactListItem } from 'components/ContactListItem/ContactListItem';
+import { getFilteredUser, getUsers } from 'redux/users/userSelectors';
+import { deleteUser } from 'redux/users/userSlice';
 
-export const ContactList = ({ contacts, onDeleteContact }) => {
+export const ContactList = () => {
+  const contactUser = useSelector(getUsers);
+  const filteredUser = useSelector(getFilteredUser);
+  const dispatch = useDispatch();
+
+  const normalizeFilteredUser = filteredUser.toLowerCase();
+  const visibleContacts = contactUser.filter(el =>
+    el.name.toLowerCase().includes(normalizeFilteredUser)
+  );
+
   return (
     <List>
-      {contacts.map(({ id, name, number }) => (
+      {visibleContacts.map(({ id, name, number }) => (
         <Item key={id}>
           <ContactListItem name={name} number={number} />
 
-          <Button type="button" onClick={() => onDeleteContact(id)}>
+          <Button type="button" onClick={() => dispatch(deleteUser(id))}>
             Delete
           </Button>
         </Item>
       ))}
     </List>
   );
-};
-
-ContactList.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    })
-  ).isRequired,
-  onDeleteContact: PropTypes.func.isRequired,
 };
